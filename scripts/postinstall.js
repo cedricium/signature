4306e4a919d9711aed51f2382c4e00cb84581d54
Cedricium <amaya.cedric@gmail.com>
2018-03-18 20:21:34 -0700

    Renamed `src/` directory to `firefox/`

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,20 +3,20 @@
 const { copySync } = require('fs-extra');
 
 const files = [
-  copySync('node_modules/quill/LICENSE', 'src/settings/vendor/quill.LICENSE'),
-  copySync('node_modules/quill/dist/quill.min.js', 'src/settings/vendor/quill.js'),
-  copySync('node_modules/quill/dist/quill.snow.css', 'src/settings/vendor/quill.snow.css'),
+  copySync('node_modules/quill/LICENSE', 'firefox/settings/vendor/quill.LICENSE'),
+  copySync('node_modules/quill/dist/quill.min.js', 'firefox/settings/vendor/quill.js'),
+  copySync('node_modules/quill/dist/quill.snow.css', 'firefox/settings/vendor/quill.snow.css'),
 
-  copySync('node_modules/quill-delta-to-html/LICENSE', 'src/content/vendor/quill-delta-to-html.LICENSE'),
-  copySync('node_modules/quill-delta-to-html/dist/browser/QuillDeltaToHtmlConverter.bundle.js', 'src/content/vendor/quill-delta-to-html.js'),
+  copySync('node_modules/quill-delta-to-html/LICENSE', 'firefox/content/vendor/quill-delta-to-html.LICENSE'),
+  copySync('node_modules/quill-delta-to-html/dist/browser/QuillDeltaToHtmlConverter.bundle.js', 'firefox/content/vendor/quill-delta-to-html.js'),
 
-  copySync('node_modules/clipboard-polyfill/LICENSE.md', 'src/content/vendor/clipboard-polyfill.LICENSE'),
-  copySync('node_modules/clipboard-polyfill/build/clipboard-polyfill.js', 'src/content/vendor/clipboard-polyfill.js'),
+  copySync('node_modules/clipboard-polyfill/LICENSE.md', 'firefox/content/vendor/clipboard-polyfill.LICENSE'),
+  copySync('node_modules/clipboard-polyfill/build/clipboard-polyfill.js', 'firefox/content/vendor/clipboard-polyfill.js'),
 
-  copySync('node_modules/quill-emoji/dist/quill-emoji.css', 'src/settings/vendor/quill-emoji.css'),
-  copySync('node_modules/quill-emoji/dist/quill-emoji.js', 'src/settings/vendor/quill-emoji.js'),
+  copySync('node_modules/quill-emoji/dist/quill-emoji.css', 'firefox/settings/vendor/quill-emoji.css'),
+  copySync('node_modules/quill-emoji/dist/quill-emoji.js', 'firefox/settings/vendor/quill-emoji.js'),
 
-  copySync('node_modules/bulma/css/bulma.css', 'src/settings/vendor/bulma.css'),
+  copySync('node_modules/bulma/css/bulma.css', 'firefox/settings/vendor/bulma.css'),
 
   // Chrome Extension
   copySync('node_modules/quill/LICENSE', 'chrome/settings/vendor/quill.LICENSE'),
