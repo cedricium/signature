6361668ad6a3bb1cc2ddc5599852800d5861c788
Cedricium <amaya.cedric@gmail.com>
2018-03-18 19:35:08 -0700

    Update chrome extension to make use of Bulma

--- chrome/settings/settings.js
+++ chrome/settings/settings.js
@@ -23,6 +23,28 @@ saveBtn.addEventListener('click', () => {
   saveSignatureToStorage(contents);
 });
 
+const clearBtn = document.querySelector('.button.clear');
+clearBtn.addEventListener('click', () => {
+  const editorLength = editor.getLength();
+  editor.deleteText(0, editorLength, 'api');
+});
+
+const hideNotificationBtn = document.querySelector('button.delete');
+const notification = hideNotificationBtn.parentElement;
+hideNotificationBtn.addEventListener('click', () => {
+  notification.classList.add('is-hidden');
+});
+
+// browser.runtime.onMessage.addListener(eventData => {
+//   switch (eventData.action) {
+//     case 'enable_warning':
+//       notification.classList.remove('is-hidden');
+//       break;
+//     default:
+//       break;
+//   }
+// });
+
 function loadSignatureFromStorage() {
   chrome.storage.local.get(null, savedData => {
     if (Object.keys(savedData).length > 0) {
